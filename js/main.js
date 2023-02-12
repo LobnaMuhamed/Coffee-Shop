b0a9912abef8101b0583e80c71d86f04dd4085ea
Lobnamuhamed <mohamedlobna306@gmail.com>
2023-02-12 02:43:47 +0200

    Update app

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 50,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  autoplay: {
+    delay: 5000,
+  },
+  speed: 1200,
 });
